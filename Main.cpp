b9ce69cbea544f6bd696d09b64018fc653ad8abc
WSZ <WSZ@DESKTOP-0KB6F8B>
2020-05-22 01:47:19 +0800

    Revert "https://github.com/WSZ9527/ADTByCpp"
    This reverts commit c21004f4f6cfbc1d0d325981c8e870c0edc682fd.

--- Main.cpp
+++ Main.cpp
@@ -3,22 +3,11 @@
 #include "ArrayListTest.hpp"
 #include "ArrayList_NewTest.hpp"
 #include <vector>
-#include <deque>
-#include "CircularArrayList.hpp"
 using namespace std;
 
 ArrayListTest t;
 ArrayList_NewTest tt;
-ArrayList<int> arr;
 int main()
 {
-	using size_type = typename CircularArrayList<int>::size_type;
-	int j = 0;
-	CircularArrayList<int> arr;
-	arr.push_back(111);
-	for (int i = 0; i < 100; ++i)
-		arr.insert(1, i+i);
-	
-	cout << arr << endl;
 	return 0;
 }
